f7286fb0e84a0583a3267be3013665f8e521084f
morswin22 <xmorswinx@gmail.com>
2019-05-16 23:13:07 +0200

    Added firebase database

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9GgEKwVrnuJoZHZuTZAJmX_LYCCAkIaw",
+    authDomain: "black-holes-6ebb2.firebaseapp.com",
+    databaseURL: "https://black-holes-6ebb2.firebaseio.com",
+    projectId: "black-holes-6ebb2",
+    storageBucket: "black-holes-6ebb2.appspot.com",
+    messagingSenderId: "748421862810",
+    appId: "1:748421862810:web:9a8f2d1d0bada2d1"
+};
+firebase.initializeApp(firebaseConfig);
---
+++ js/subscription.js
@@ -0,0 +1,40 @@
+(function() {
+    const subButton = document.querySelector('#newsletter button');
+    const input = document.querySelector('#newsletter input');
+    const defaultMessage = input.validationMessage;
+    const database = firebase.database();
+    
+    subButton.addEventListener("click", function () {
+        if (input.className == 'revealed') {
+            if (input.value.length > 0 && input.checkValidity()) {
+                database.ref('/users/').once('value', function(e){
+                    let users = e.exportVal();
+                    let ok = true;
+                    for (let id in users) {
+                        if (users[id].email == input.value) ok = false;
+                    }
+                    if (ok) {
+                        database.ref('/users/').push({email: input.value});
+                        // tell that user was accepted
+                    } else {
+                        input.setCustomValidity("This email address is already subscribed to our newsletter");
+                        input.reportValidity();
+                    }
+                });
+            } else {
+                input.setCustomValidity(defaultMessage);
+                input.reportValidity();
+            }
+        } else {
+            input.classList.add('revealed');
+            input.focus();
+            subButton.innerHTML = 'Submit';
+        }
+    });
+
+    input.addEventListener("keyup", function(e) {
+        if (e.keyCode == 13) {
+            subButton.click();
+        }
+    });
+})();
